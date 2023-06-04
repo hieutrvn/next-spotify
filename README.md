e61ab7ee7ca53896f7f53d974d010931b1d8671e
Hieu Tran <tranhieuhalai@gmail.com>
2023-06-04 21:21:48 +0700

    fix types

--- README.md
+++ README.md
@@ -21,10 +21,6 @@ Key Features:
 - Favorites system
 - Playlists / Liked songs system
 
-### Prerequisites
-
-**Node version 14.x**
-
 ### Cloning the repository
 
 ```shell
@@ -45,20 +41,8 @@ NEXT_PUBLIC_SUPABASE_ANON_KEY=
 SUPABASE_SERVICE_ROLE_KEY=
 ```
 
-### Add SQL Tables
-
-Use `database.sql` file, create songs and liked_songs table (there is a video tutorial)
-
 ### Start the app
 
 ```shell
 npm run dev
 ```
-
-## Available commands
-
-Running commands with npm `npm run [command]`
-
-| command | description                              |
-| :------ | :--------------------------------------- |
-| `dev`   | Starts a development instance of the app |
--- types.ts
+++ types.ts
@@ -1,12 +1,9 @@
-import Stripe from "stripe"
-
 export interface Product {
     id: string;
     active?: boolean;
     name?: string;
     description?: string;
     image?: string;
-    metadata?: Stripe.Metadata;
 }
 
 export interface UserDetails {
@@ -15,15 +12,11 @@ export interface UserDetails {
     last_name: string;
     full_name?: string;
     avatar_url?: string;
-    billing_address?: Stripe.Address;
-    payment_method?: Stripe.PaymentMethod[Stripe.PaymentMethod.Type];
 }
 
 export interface Subscription {
     id: string;
     user_id: string;
-    status?: Stripe.Subscription.Status;
-    metadata?: Stripe.Metadata;
     price_id?: string;
     quantity?: number;
     cancel_at_period_end?: boolean;
@@ -35,23 +28,8 @@ export interface Subscription {
     canceled_at?: string;
     trial_start?: string;
     trial_end?: string;
-    prices?: Price;
 }
 
-export interface Price {
-    id: string;
-    product_id?: string;
-    active?: boolean;
-    description?: string;
-    unit_amount?: number;
-    currency?: string;
-    type?: Stripe.Price.Type;
-    interval?: Stripe.Price.Recurring.Interval;
-    interval_count?: number;
-    trial_period_days?: number | null;
-    metadata?: Stripe.Metadata;
-    products?: Product;
-}
 
 export interface Product {
     id: string;
@@ -59,7 +37,6 @@ export interface Product {
     name?: string;
     description?: string;
     image?: string;
-    metadata?: Stripe.Metadata;
 }
 
 export interface Song {
